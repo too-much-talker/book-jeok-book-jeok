25a7833fc70b55061db16c898433bd3ce24fabbe
이재경 <aletsire@gmail.com>
2022-01-21 02:16:03 +0000

    [BE: 프로젝트 세팅] #S06P12A305-8 프로젝트 세팅

---
+++ backend/src/test/java/com/ssafy/bjbj/BjbjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bjbj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BjbjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
