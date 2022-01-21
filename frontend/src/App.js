a494a1086a2f97402ce3a87b8270cac52a850ffa
unain-dev <daeun84366@gmail.com>
2022-01-21 17:05:26 +0900

    #S06P12A305-25 Create: 디렉토리 추가 및 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,28 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./views/user/login/LoginPresenter";
+import Signup from "./views/user/signup/SignupPresenter";
+import MyPage from "./views/user/myPage/index";
+import Home from "./views/main/Home";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/mypage">
+          <MyPage />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
