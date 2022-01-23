1e67e438af3451939f850fc42cc8b80dfe9d9ff3
김경석 <0xe82de@gmail.com>
2022-01-23 15:12:24 +0900

    #S06P12A305-11 Feat: 첨부 파일 정보 엔티티 필드 추가
    - 식별자, 원본 이름, 인코딩된 이름, 저장 경로, 삭제 여부
    - 회원 다대일 매핑

--- backend/src/main/java/com/ssafy/bjbj/common/entity/FileInfo.java
+++ backend/src/main/java/com/ssafy/bjbj/common/entity/FileInfo.java
@@ -1,11 +1,13 @@
 package com.ssafy.bjbj.common.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
@@ -13,4 +15,21 @@ import javax.persistence.Table;
 @Table(name = "tb_file_info")
 @Entity
 public class FileInfo extends BaseLastModifiedEntity {
+
+    @Column(name = "file_info_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    private String originFileName;
+
+    private String encodedFileName;
+
+    private String savedPath;
+
+    private boolean isDeleted;
+
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
 }
