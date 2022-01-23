3d47be7e6d8b0474d9e3b2ff13ec1ed6848c7f6b
김은선 <kus0412@naver.com>
2022-01-23 20:16:08 +0900

    #S06P12A305-11 Feat: 챌린지 인증 엔티티 필드 추가
    - 식별자, 제목, 본문, 삭제여부
    - 회원 다대일 매핑
    - 챌린지 다대일 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/challenge/entity/ChallengeAuth.java
+++ backend/src/main/java/com/ssafy/bjbj/api/challenge/entity/ChallengeAuth.java
@@ -1,11 +1,13 @@
 package com.ssafy.bjbj.api.challenge.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
@@ -13,4 +15,25 @@ import javax.persistence.Table;
 @Table(name = "tb_challenge_auth")
 @Entity
 public class ChallengeAuth {
+
+    @Column(name = "challenge_auth_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Challenge challenge;
 }
