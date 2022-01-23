48f3f35276a289cf248bed0c704646bf254d2ff9
김경석 <0xe82de@gmail.com>
2022-01-23 11:44:37 +0900

    #S06P12A305-11 Create: 북로그 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/booklog/entity/Booklog.java
@@ -0,0 +1,17 @@
+package com.ssafy.bjbj.api.booklog.entity;
+
+import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+//@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_booklog")
+@Entity
+public class Booklog extends BaseLastModifiedEntity {
+}
