13d2c89c19dd0df4bdca792dcdaab29ce744c443
김경석 <0xe82de@gmail.com>
2022-01-23 12:45:34 +0900

    #S06P12A305-11 Feat: 회원 엔티티 필드 추가
    - 식별자, 이메일, 패스워드, 이름, 닉네임, 역할, 포인트, 경험치, 삭제 여부 필드 추가
    - 북로그 일대다 매핑
    - 북리뷰 일대다 매핑
    - 좋아요 일대다 매핑
    - 구독 다대다 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/member/entity/Member.java
+++ backend/src/main/java/com/ssafy/bjbj/api/member/entity/Member.java
@@ -1,13 +1,78 @@
 package com.ssafy.bjbj.api.member.entity;
 
+import com.ssafy.bjbj.api.bookinfo.entity.BookReview;
+import com.ssafy.bjbj.api.booklog.entity.Booklog;
+import com.ssafy.bjbj.api.booklog.entity.Like;
 import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
 import lombok.*;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_member")
 @Entity
 public class Member extends BaseLastModifiedEntity {
+
+    @Column(name = "member_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(nullable = false)
+    private Integer point;
+
+    @Column(nullable = false)
+    private Integer exp;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @Column(nullable = false)
+    @JoinColumn(name = "booklog_id")
+    @OneToMany
+    private List<Booklog> booklogs = new ArrayList<>();
+
+    @JoinColumns({
+            @JoinColumn(name = "book_info_id", referencedColumnName = "book_info_id"),
+            @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+    })
+    @OneToMany
+    private List<BookReview> bookReviews = new ArrayList<>();
+
+    @JoinColumns({
+            @JoinColumn(name = "booklog_id", referencedColumnName = "booklog_id"),
+            @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+    })
+    @OneToMany
+    private List<Like> likes = new ArrayList<>();
+
+    // 나를 구독한 사람들
+    @JoinColumn(name = "to_member_id")
+    @OneToMany
+    private List<Subscribe> fromMembers = new ArrayList<>();
+
+    // 내가 구독한 사람들
+    @JoinColumn(name = "from_member_id")
+    @OneToMany
+    private List<Subscribe> toMembers = new ArrayList<>();
+
 }
