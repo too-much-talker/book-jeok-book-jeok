3d3508d25b17bfa3ffb42c9d05ac560e0bb367bb
김경석 <0xe82de@gmail.com>
2022-01-23 14:59:25 +0900

    #S06P12A305-11 Create: 공지사항 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/notice/entity/Notice.java
@@ -0,0 +1,17 @@
+package com.ssafy.bjbj.api.notice.entity;
+
+import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+//@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_notice")
+@Entity
+public class Notice extends BaseLastModifiedEntity {
+}
