54a75df13f7b6ffb75d6f21394512678a8478265
김은선 <kus0412@naver.com>
2022-01-23 20:17:56 +0900

    #S06P12A305-11 Feat: 포인트 기록 엔티티 필드 추가
    - 식별자, 포인트 변동량, 포인트 변동 사유
    - 회원 식별자 다대일 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/challenge/entity/PointHistory.java
+++ backend/src/main/java/com/ssafy/bjbj/api/challenge/entity/PointHistory.java
@@ -1,16 +1,34 @@
 package com.ssafy.bjbj.api.challenge.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
+import com.ssafy.bjbj.common.entity.BaseCreatedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tb_point_history")
 @Entity
-public class PointHistory {
+public class PointHistory extends BaseCreatedEntity {
+    @Column(name = "point_history_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer pointDiff;
+
+    @Column(nullable = false)
+    private PointDiffReason pointDiffReason;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
 }
