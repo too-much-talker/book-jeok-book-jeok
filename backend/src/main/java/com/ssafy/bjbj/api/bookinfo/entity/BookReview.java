28f60b3cdf3de3794f6a0fa813af67d7a0caa833
김경석 <0xe82de@gmail.com>
2022-01-23 12:08:16 +0900

    #S06P12A305-11 Create: 책 리뷰 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookReview.java
@@ -0,0 +1,17 @@
+package com.ssafy.bjbj.api.bookinfo.entity;
+
+import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+//@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_book_review")
+@Entity
+public class BookReview extends BaseLastModifiedEntity {
+}
