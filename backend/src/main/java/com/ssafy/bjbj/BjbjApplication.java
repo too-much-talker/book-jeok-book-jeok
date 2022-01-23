0285183d2fd4b1881900dd380e3fccd05715b58b
김경석 <0xe82de@gmail.com>
2022-01-23 12:44:03 +0900

    #S06P12A305-11 Feat: JPA Auditing 활성화 및 UUID 반환
    추후에 UUID를 회원 식별자 등으로 대체해야 합니다.

--- backend/src/main/java/com/ssafy/bjbj/BjbjApplication.java
+++ backend/src/main/java/com/ssafy/bjbj/BjbjApplication.java
@@ -2,12 +2,24 @@ package com.ssafy.bjbj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.Optional;
+import java.util.UUID;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class BjbjApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BjbjApplication.class, args);
+	}
+
+	@Bean
+	public AuditorAware<String> auditorProvider() {
+		return () -> Optional.of(UUID.randomUUID().toString());
 	}
 
 }
