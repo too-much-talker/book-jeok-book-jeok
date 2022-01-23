2c3455fe55a2f300c6e3009d37afaf7e5884c1dd
김경석 <0xe82de@gmail.com>
2022-01-24 00:31:10 +0900

    #S06P12A305-11 Feat: 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ExpHistory.java
+++ backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ExpHistory.java
@@ -1,12 +1,14 @@
 package com.ssafy.bjbj.api.readinggroup.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.common.entity.base.BaseCreatedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
@@ -14,4 +16,21 @@ import javax.persistence.Table;
 @Table(name = "tb_exp_history")
 @Entity
 public class ExpHistory extends BaseCreatedEntity {
+
+    @Column(name = "exp_history_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer expDiff;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ExpDiffReason expDiffReason;
+
+    @JoinColumn(name = "member_id", nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
 }
