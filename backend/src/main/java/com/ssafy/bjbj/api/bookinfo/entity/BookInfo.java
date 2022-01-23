eb27ab9b2ac8e609a25a6ebe28b3f9931a66fab7
김경석 <0xe82de@gmail.com>
2022-01-23 12:49:49 +0900

    #S06P12A305-11 Feat: 책정보 엔티티 필드 추가
    - 식별자, 제목, 권차, 총서명, 총서평차, 저자, ISBN, 세트 표현, 발행처, 페이지, 출판일, 주제, 표지 이미 URL, 책 소개, 책 요약, 등록 날짜, 수정 날짜, 별점, 삭제 여부
    - 북로그 일대다 매핑
    - 책리뷰 일대다 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookInfo.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookInfo.java
@@ -1,12 +1,15 @@
 package com.ssafy.bjbj.api.bookinfo.entity;
 
+import com.ssafy.bjbj.api.booklog.entity.Booklog;
 import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
@@ -14,4 +17,83 @@ import javax.persistence.Table;
 @Table(name = "tb_book_info")
 @Entity
 public class BookInfo extends BaseLastModifiedEntity {
+
+    @Column(name = "book_info_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Integer vol;
+
+    @Column(nullable = false)
+    private String seriesTitle;
+
+    @Column(nullable = false)
+    private Integer seriesNo;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private String eaIsbn;
+
+    @Column(nullable = false)
+    private String eaAddCode;
+
+    @Column(nullable = false)
+    private String setIsbn;
+
+    @Column(nullable = false)
+    private String setAddCode;
+
+    @Column(nullable = false)
+    private boolean setExpression;
+
+    @Column(nullable = false)
+    private String publisher;
+
+    @Column(nullable = false)
+    private Integer page;
+
+    @Column(nullable = false)
+    private LocalDateTime publishDate;
+
+    @Column(nullable = false)
+    private String subject;
+
+    @Column(nullable = false)
+    private String titleUrl;
+
+    @Column(nullable = false)
+    private String bookIntroductionUrl;
+
+    @Column(nullable = false)
+    private String bookSummaryUrl;
+
+    @Column(nullable = false)
+    private LocalDateTime inputDate;
+
+    @Column(nullable = false)
+    private LocalDateTime updateDate;
+
+    @Column(nullable = false)
+    private Double starRating;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @OneToMany(mappedBy = "bookInfo")
+    private List<Booklog> booklogs = new ArrayList<>();
+
+    @JoinColumns({
+            @JoinColumn(name = "book_info_id", referencedColumnName = "book_info_id"),
+            @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+    })
+    @OneToMany
+    private List<BookReview> bookReviews = new ArrayList<>();
+
 }
