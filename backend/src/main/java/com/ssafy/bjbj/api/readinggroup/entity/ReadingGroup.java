8111bce65fd450e02d1d334f75c67ade8056e22a
김경석 <0xe82de@gmail.com>
2022-01-24 00:32:32 +0900

    #S06P12A305-11 Fix: 필드 수정
    - @Column(nullable = false) 추가
    - 상태 이넘 초기화
    - 회원, 독서모임 게시판, 독서모임 날짜, 독서모임 회원 외래키 매핑 수정

--- backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ReadingGroup.java
+++ backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ReadingGroup.java
@@ -1,8 +1,8 @@
 package com.ssafy.bjbj.api.readinggroup.entity;
 
-import com.ssafy.bjbj.api.booklog.entity.Like;
 import com.ssafy.bjbj.api.member.entity.Member;
-import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
+import com.ssafy.bjbj.common.entity.Status;
+import com.ssafy.bjbj.common.entity.base.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,52 +20,62 @@ import static javax.persistence.FetchType.LAZY;
 @Table(name = "tb_reading_group")
 @Entity
 public class ReadingGroup extends BaseLastModifiedEntity {
+
     @Column(name = "reading_group_id")
     @GeneratedValue
     @Id
     private Long id;
 
+    @Column(nullable = false)
     private String title;
 
+    @Column(nullable = false)
     private String content;
 
+    @Column(nullable = false)
     private Integer views;
 
+    @Column(nullable = false)
     private Integer limitPoint;
 
+    @Column(nullable = false)
     private Integer maxMember;
 
+    @Column(nullable = false)
     private LocalDateTime deadline;
 
-    private Status status;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status = Status.PRE;
 
+    @Column(nullable = false)
     private LocalDateTime startDate;
 
+    @Column(nullable = false)
     private LocalDateTime endDate;
 
-    private Type type;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ReadingGroupType readingGroupType;
 
+    @Column(nullable = false)
     private boolean isDeleted;
 
-    @JoinColumn(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     @ManyToOne(fetch = LAZY)
     private Member member;
 
-    @JoinColumn(name = "reading_group_board_id")
+    @JoinColumn(name = "reading_group_id")
     @OneToMany
     private List<ReadingGroupBoard> readingGroupBoards = new ArrayList<>();
 
-    @JoinColumn(name = "reading_group_board_comments_id")
+    @JoinColumn(name = "reading_group_id")
     @OneToMany
-    private List<ReadingGroupBoardComment> readingGroupBoardComments = new ArrayList<>();
-
-    @OneToMany(mappedBy = "readingGroup")
     private List<ReadingGroupDate> readingGroupDates = new ArrayList<>();
 
-    @JoinColumns({
-            @JoinColumn(name = "reading_group_id", referencedColumnName = "reading_group_id"),
-            @JoinColumn(name = "member_id", referencedColumnName = "member_id")
-    })
+    // 독서모임에 속한 멤버들
+    @JoinColumn(name = "reading_group_id")
     @OneToMany
     private List<ReadingGroupMember> readingGroupMembers = new ArrayList<>();
+
 }
