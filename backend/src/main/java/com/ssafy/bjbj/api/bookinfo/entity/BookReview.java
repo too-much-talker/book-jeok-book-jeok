eceb45387545caa9b7bae4f6ede731e8d33acdd4
김경석 <0xe82de@gmail.com>
2022-01-23 12:50:47 +0900

    #S06P12A305-11 Feat: 책리뷰 엔티티 필드 추가
    - 책정보 식별자, 회원 식별자 복합키, 다대다 매핑
    - 별점, 한줄평, 삭제여부

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookReview.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookReview.java
@@ -1,12 +1,12 @@
 package com.ssafy.bjbj.api.bookinfo.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
@@ -14,4 +14,24 @@ import javax.persistence.Table;
 @Table(name = "tb_book_review")
 @Entity
 public class BookReview extends BaseLastModifiedEntity {
+
+    @Column(name = "book_info_id")
+    @ManyToOne
+    @Id
+    private BookInfo bookInfo;
+
+    @Column(name = "member_id")
+    @ManyToOne
+    @Id
+    private Member member;
+
+    @Column(nullable = false)
+    private Integer starRating;
+
+    @Column(nullable = false)
+    private String summary;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
 }
