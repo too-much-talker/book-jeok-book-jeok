0e3317af8e744a72c69a00d8a3210cf905f49342
aletsire <aletsire@gmail.com>
2022-01-23 17:45:47 +0900

    #S06P12A305-11 Feat: 독서모임 회원 엔티티 필드 추가
    - 회원, 독서모임 모집 다대일 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ReadingGroupMember.java
+++ backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ReadingGroupMember.java
@@ -1,18 +1,28 @@
 package com.ssafy.bjbj.api.readinggroup.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@Table(name = "tb_reading_group_date")
+@Table(name = "tb_reading_group_Member")
 @Entity
 public class ReadingGroupMember extends BaseLastModifiedEntity {
-    
+
+    @Column(name = "reading_group_id")
+    @ManyToOne
+    @Id
+    private ReadingGroup readingGroup;
+
+    @Column(name = "member_id")
+    @ManyToOne
+    @Id
+    private Member member;
+
 }
