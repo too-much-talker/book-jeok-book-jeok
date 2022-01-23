aaf9ecc62d14dcd06e57f126e6a81e343b485589
김경석 <0xe82de@gmail.com>
2022-01-24 00:13:05 +0900

    #S06P12A305-11 Rename: 이름 변경

--- backend/src/main/java/com/ssafy/bjbj/api/member/entity/Type.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.bjbj.api.member.entity;
-
-public enum Type {
-    BOOKLOG, CHALLENGE, READING_GROUP
-}
--- backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/Type.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.bjbj.api.readinggroup.entity;
-
-public enum Type {
-    PROFESSIONAL, FRIENDLY, CASUAL
-}
