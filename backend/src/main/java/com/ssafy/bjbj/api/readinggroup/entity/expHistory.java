6275392b39a4d3d059d22b942765d1e1476042d8
aletsire <aletsire@gmail.com>
2022-01-23 17:46:44 +0900

    #S06P12A305-11 Feat: 경험치 기록 엔티티 필드 추가
    - 경험치 변동량, 경험치 변동 사유
    - 회원 다대일 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/expHistory.java
+++ backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/expHistory.java
@@ -1,5 +1,6 @@
 package com.ssafy.bjbj.api.readinggroup.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.common.entity.BaseCreatedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -7,11 +8,25 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Getter
 //@ToString(of = {"id", "username", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tb_exp_history")
 @Entity
-public class expHistory extends BaseCreatedEntity {
+public class ExpHistory extends BaseCreatedEntity {
 
+    @Column(name = "exp_history_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    private Integer expDiff;
+
+    private ExpDiffReason expDiffReason;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = LAZY)
+    private Member member;
 }
