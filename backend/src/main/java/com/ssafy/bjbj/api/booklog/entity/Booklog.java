7b93bb7bf50bbdb6b3224ad43d53b6fe91bfbafb
김경석 <0xe82de@gmail.com>
2022-01-23 12:47:49 +0900

    #S06P12A305-11 Feat: 북로그 엔티티 필드 추가
    - 식별자, 제목, 본문, 한줄평, 별점, 완독일, 공개여부, 삭제여부
    - 회원 다대일 매핑
    - 책정보 다대일 매핑
    - 좋아요 일대다 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/booklog/entity/Booklog.java
+++ backend/src/main/java/com/ssafy/bjbj/api/booklog/entity/Booklog.java
@@ -1,12 +1,18 @@
 package com.ssafy.bjbj.api.booklog.entity;
 
+import com.ssafy.bjbj.api.bookinfo.entity.BookInfo;
+import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
@@ -14,4 +20,43 @@ import javax.persistence.Table;
 @Table(name = "tb_booklog")
 @Entity
 public class Booklog extends BaseLastModifiedEntity {
+
+    @Column(name = "booklog_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    private String summary;
+
+    private Integer starRating;
+
+    private LocalDateTime readDate;
+
+    @Column(nullable = false)
+    private boolean isOpen;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @Column(nullable = false)
+    @JoinColumn(name = "book_info_id")
+    @ManyToOne(fetch = LAZY)
+    private BookInfo bookInfo;
+
+    @JoinColumns({
+            @JoinColumn(name = "booklog_id", referencedColumnName = "booklog_id"),
+            @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+    })
+    @OneToMany
+    private List<Like> likes = new ArrayList<>();
+
 }
