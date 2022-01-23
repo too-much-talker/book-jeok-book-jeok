16334ac53286431e7e2876fafa7b48ecbd9f4b43
김은선 <kus0412@naver.com>
2022-01-23 20:14:48 +0900

    #S06P12A305-11 Feat: 챌린지 엔티티 필드 추가
    - 식별자, 제목, 본문, 조회수, 모집기한, 시작일, 종료일, 인증요구횟수, 제한 인원, 리워드, 상태, 삭제여부
    - 회원 다대일 매핑
    - 챌린지 인증 일대다 매핑
    - 챌린지 회원 일대다 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/challenge/entity/Challenge.java
+++ backend/src/main/java/com/ssafy/bjbj/api/challenge/entity/Challenge.java
@@ -1,16 +1,78 @@
 package com.ssafy.bjbj.api.challenge.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
+import com.ssafy.bjbj.api.readinggroup.entity.Status;
+import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "tb_challenge")
 @Entity
-public class Challenge {
+public class Challenge extends BaseLastModifiedEntity {
+
+    @Column(name = "challenge_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private Integer views;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+    @Column(nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(nullable = false)
+    private Integer requiredAuth;
+
+    @Column(nullable = false)
+    private Integer maxMember;
+
+    @Column(nullable = false)
+    private Integer reward;
+
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false)
+    private boolean isDeleted;
+
+    @Column(nullable = false)
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @Column(nullable = false)
+    @JoinColumn(name = "challenge_auth_id")
+    @OneToMany
+    private List<ChallengeAuth> challengeAuths = new ArrayList<>();
+
+    @JoinColumns({
+            @JoinColumn(name = "challenge_id", referencedColumnName = "challenge_id"),
+            @JoinColumn(name = "member_id", referencedColumnName = "member_id")
+    })
+    @OneToMany
+    private List<ChallengeMember> challengeMembers = new ArrayList<>();
+
 }
