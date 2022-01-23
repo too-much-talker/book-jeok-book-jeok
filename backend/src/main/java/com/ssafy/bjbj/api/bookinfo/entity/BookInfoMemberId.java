32027cad033531dd3a8bdbe73343284823a275c4
김경석 <0xe82de@gmail.com>
2022-01-24 00:21:23 +0900

    #S06P12A305-11 Feat: 책리뷰 엔티티에 책정보, 회원 복합키 설정

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookInfoMemberId.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookInfoMemberId.java
@@ -1,6 +1,24 @@
 package com.ssafy.bjbj.api.bookinfo.entity;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class BookInfoMemberId implements Serializable {
+
+    private Long bookInfo;
+    private Long member;
+    
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookInfoMemberId that = (BookInfoMemberId) o;
+        return Objects.equals(bookInfo, that.bookInfo) && Objects.equals(member, that.member);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookInfo, member);
+    }
+
 }
--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookReview.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/entity/BookReview.java
@@ -1,26 +1,28 @@
 package com.ssafy.bjbj.api.bookinfo.entity;
 
 import com.ssafy.bjbj.api.member.entity.Member;
-import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
+import com.ssafy.bjbj.common.entity.base.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@IdClass(BookInfoMemberId.class)
 @Table(name = "tb_book_review")
 @Entity
 public class BookReview extends BaseLastModifiedEntity {
 
-    @Column(name = "book_info_id")
+    @JoinColumn(name = "book_info_id", nullable = false)
     @ManyToOne
     @Id
     private BookInfo bookInfo;
 
-    @Column(name = "member_id")
+    @JoinColumn(name = "member_id", nullable = false)
     @ManyToOne
     @Id
     private Member member;
