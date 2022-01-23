1c6370d4c431727235aa5df317e35ce21b42ce6f
aletsire <aletsire@gmail.com>
2022-01-23 17:42:37 +0900

    #S06P12A305-11 Feat: 독서모임 게시판 엔티티 필드 추가
    - 제목, 본문, 조회수, 삭제여부
    - 회원, 독서모임 모집 다대일 매핑
    - 독서모임 댓글 일대다 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ReadingGroupBoard.java
+++ backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ReadingGroupBoard.java
@@ -1,4 +1,46 @@
 package com.ssafy.bjbj.api.readinggroup.entity;
 
-public class ReadingGroupBoard {
+import com.ssafy.bjbj.api.member.entity.Member;
+import com.ssafy.bjbj.api.notice.entity.NoticeComment;
+import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+//@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_reading_group_board")
+@Entity
+public class ReadingGroupBoard extends BaseLastModifiedEntity {
+
+    @Column(name = "reading_group_board_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Integer views;
+
+    private boolean isDeleted;
+
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    private ReadingGroup readingGroup;
+
+    @JoinColumn(name = "reading_group_board_comment_id")
+    @OneToMany
+    private List<ReadingGroupBoardComment> readingGroupBoardComments = new ArrayList<>();
 }
