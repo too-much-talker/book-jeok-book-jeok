1a612e84b2b99277b1893ebd6e04257f23a7728f
김경석 <0xe82de@gmail.com>
2022-01-23 15:14:45 +0900

    #S06P12A305-11 Feat: 공지사항 엔티티 필드 추가
    - 식별자, 제목, 본문, 조회수, 삭제 여부
    - 회원 다대일 매핑
    - 공지사항 일대다 매핑

--- backend/src/main/java/com/ssafy/bjbj/api/notice/entity/Notice.java
+++ backend/src/main/java/com/ssafy/bjbj/api/notice/entity/Notice.java
@@ -1,12 +1,17 @@
 package com.ssafy.bjbj.api.notice.entity;
 
+import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.*;
 
 @Getter
 //@ToString(of = {"id", "username", "age"})
@@ -14,4 +19,26 @@ import javax.persistence.Table;
 @Table(name = "tb_notice")
 @Entity
 public class Notice extends BaseLastModifiedEntity {
+
+    @Column(name = "notice_id")
+    @GeneratedValue
+    @Id
+    private Long id;
+
+    private String title;
+
+    private String content;
+
+    private Integer views;
+
+    private boolean isDeleted;
+
+    @JoinColumn(name = "member_id")
+    @ManyToOne(fetch = LAZY)
+    private Member member;
+
+    @JoinColumn(name = "notice_comment_id")
+    @OneToMany
+    private List<NoticeComment> noticeComments = new ArrayList<>();
+
 }
