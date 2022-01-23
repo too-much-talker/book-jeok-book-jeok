a9cf0df96290caa209376bf7fadbb69c436b96fa
김경석 <0xe82de@gmail.com>
2022-01-23 11:30:43 +0900

    #S06P12A305-11 Create: last_modified_date, last_modified_by 기본 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/common/entity/BaseLastModifiedEntity.java
@@ -0,0 +1,13 @@
+package com.ssafy.bjbj.common.entity;
+
+import lombok.Getter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseLastModifiedEntity {
+}
