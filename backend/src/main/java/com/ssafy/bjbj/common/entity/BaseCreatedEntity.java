b53bb8db9bd1ae13064212c7373d6a80b1d80074
김경석 <0xe82de@gmail.com>
2022-01-23 12:43:04 +0900

    #S06P12A305-11 Feat: 생성자, 생성일 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/common/entity/BaseCreatedEntity.java
+++ backend/src/main/java/com/ssafy/bjbj/common/entity/BaseCreatedEntity.java
@@ -1,13 +1,26 @@
 package com.ssafy.bjbj.common.entity;
 
 import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import javax.persistence.Column;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
 
 @Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class BaseCreatedEntity {
+public abstract class BaseCreatedEntity {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+
+    @CreatedBy
+    @Column(updatable = false)
+    private String createdBy;
+
 }
