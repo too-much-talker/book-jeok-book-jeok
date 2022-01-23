c7b9af059b2ca15539f3322c7de35b6388b8fd7e
aletsire <aletsire@gmail.com>
2022-01-23 16:19:52 +0900

    #S06P12A305-11 Create: 독서모임 엔티티 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/readinggroup/entity/ReadingGroup.java
@@ -0,0 +1,22 @@
+package com.ssafy.bjbj.api.readinggroup.entity;
+
+import com.ssafy.bjbj.api.member.entity.Member;
+import com.ssafy.bjbj.common.entity.BaseLastModifiedEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Getter
+//@ToString(of = {"id", "username", "age"})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tb_reading_group")
+@Entity
+public class ReadingGroup extends BaseLastModifiedEntity {
+}
