aabaeebdd109d78bfb55aba9034f750ee09c0c7c
김경석 <0xe82de@gmail.com>
2022-01-24 15:18:11 +0900

    #S06P12A305-26 Create: 회원 Repository 인터페이스 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/member/repository/MemberRepository.java
@@ -0,0 +1,7 @@
+package com.ssafy.bjbj.api.member.repository;
+
+import com.ssafy.bjbj.api.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
