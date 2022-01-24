bb43768c1db705227a929b2e525fffca1fa98c0c
김경석 <0xe82de@gmail.com>
2022-01-24 15:29:21 +0900

    #S06P12A305-26 Feat: 이메일 존재 확인 메서드 선언부 추가

--- backend/src/main/java/com/ssafy/bjbj/api/member/repository/MemberRepository.java
+++ backend/src/main/java/com/ssafy/bjbj/api/member/repository/MemberRepository.java
@@ -4,4 +4,7 @@ import com.ssafy.bjbj.api.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    boolean existsByEmail(String email);
+
 }
