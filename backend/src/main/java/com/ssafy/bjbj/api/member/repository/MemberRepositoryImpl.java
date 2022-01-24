0f6b1734d757938d6f9d7482fa283a7f7af5a601
aletsire <aletsire@gmail.com>
2022-01-25 00:20:26 +0900

    #S06P12A305-27 Feature: 멤버 서비스 Impl 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/member/repository/MemberRepositoryImpl.java
+++ backend/src/main/java/com/ssafy/bjbj/api/member/repository/MemberRepositoryImpl.java
@@ -1,5 +1,33 @@
 package com.ssafy.bjbj.api.member.repository;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.bjbj.api.member.dto.MemberDto;
+import com.ssafy.bjbj.api.member.dto.QMemberDto;
+import com.ssafy.bjbj.api.member.entity.QMember;
+
+import javax.persistence.EntityManager;
+
+import static com.ssafy.bjbj.api.member.entity.QMember.*;
+
 public class MemberRepositoryImpl implements MemberRepositoryCustom {
 
+    private final JPAQueryFactory queryFactory;
+
+    public MemberRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public MemberDto findMemberDtoByEmail(String email) {
+
+        return queryFactory
+                .select(new QMemberDto(
+                        member.email,
+                        member.password,
+                        member.name,
+                        member.nickname))
+                .from(member)
+                .where(member.email.eq(email))
+                .fetchOne();
+    }
 }
