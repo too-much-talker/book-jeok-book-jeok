108fde7baa065425196dc41e825ab2bf7837e903
김경석 <0xe82de@gmail.com>
2022-01-24 16:14:05 +0900

    #S06P12A305-26 Create: 회원 Repository 테스트 클래스 생성

---
+++ backend/src/test/java/com/ssafy/bjbj/api/member/repository/MemberRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.ssafy.bjbj.api.member.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class MemberRepositoryTest {
+
+}
