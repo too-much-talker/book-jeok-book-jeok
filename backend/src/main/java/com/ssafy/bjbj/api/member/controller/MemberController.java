b67f0209cd82356645bffa4e548626caf747d5a3
김경석 <0xe82de@gmail.com>
2022-01-24 11:20:20 +0900

    #S06P12A305-26 Create: 회원 컨트롤러 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/member/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.ssafy.bjbj.api.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/members")
+@RestController
+public class MemberController {
+}
