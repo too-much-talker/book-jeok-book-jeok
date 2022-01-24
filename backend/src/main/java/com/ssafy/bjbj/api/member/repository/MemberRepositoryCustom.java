c5ac50a5a99da0b507be8b2a31cb09c9130dbbdb
aletsire <aletsire@gmail.com>
2022-01-25 00:20:14 +0900

    #S06P12A305-27 Feature: 멤버 레포지토리 커스텀 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/member/repository/MemberRepositoryCustom.java
+++ backend/src/main/java/com/ssafy/bjbj/api/member/repository/MemberRepositoryCustom.java
@@ -3,5 +3,6 @@ package com.ssafy.bjbj.api.member.repository;
 import com.ssafy.bjbj.api.member.dto.MemberDto;
 
 public interface MemberRepositoryCustom {
-    
+
+    MemberDto findMemberDtoByEmail(String email);
 }
