1de1fa292e7aa15cc7fde65a8fa3dc7b5de98f35
김경석 <0xe82de@gmail.com>
2022-01-25 22:19:57 +0900

    #S06P12A305-96 Feat: 응답용 회원 Dto 필드 추가 및 Querydsl 생성자 추가

--- backend/src/main/java/com/ssafy/bjbj/api/member/dto/response/ResponseMemberDto.java
+++ backend/src/main/java/com/ssafy/bjbj/api/member/dto/response/ResponseMemberDto.java
@@ -1,13 +1,37 @@
 package com.ssafy.bjbj.api.member.dto.response;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-@ToString(of = "id", "email", "name", "nickname", "phoneNumber")
+@ToString(of = {"id", "email", "password", "name", "nickname", "phoneNumber"})
 @NoArgsConstructor
 @Getter
 @Builder
 public class ResponseMemberDto {
+
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private String name;
+
+    private String nickname;
+
+    private String phoneNumber;
+
+    @QueryProjection
+    public ResponseMemberDto(Long id, String email, String password, String name, String nickname, String phoneNumber) {
+        this.id = id;
+        this.password = password;
+        this.email = email;
+        this.name = name;
+        this.nickname = nickname;
+        this.phoneNumber = phoneNumber;
+    }
+
 }
