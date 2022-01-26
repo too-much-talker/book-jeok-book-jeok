582ae5e5c6982260bc0987c58e6da8327f19c6d2
aletsire <aletsire@gmail.com>
2022-01-26 17:37:51 +0900

    #S06P12A305-89 Feat: 회원 서비스 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/member/service/MemberService.java
+++ backend/src/main/java/com/ssafy/bjbj/api/member/service/MemberService.java
@@ -2,6 +2,7 @@ package com.ssafy.bjbj.api.member.service;
 
 import com.ssafy.bjbj.api.member.dto.request.RequestMemberDto;
 import com.ssafy.bjbj.api.member.entity.Member;
+import com.ssafy.bjbj.api.member.entity.Subscribe;
 
 public interface MemberService {
 
@@ -14,5 +15,11 @@ public interface MemberService {
     Member findMemberByEmail(String email);
 
     RequestMemberDto findMemberDtoByEmail(String email);
+
+    Member findMemberById(Long id);
+
+    boolean subscribeMember(Long fromMemberId, Long toMemberId);
+
+    boolean unsubscribeMember(Long fromMemberId, Long toMemberId);
 
 }
