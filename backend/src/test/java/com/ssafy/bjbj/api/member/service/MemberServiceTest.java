7af980b0307763ffde1c6c436904c54a42b17436
aletsire <aletsire@gmail.com>
2022-01-26 17:42:09 +0900

    #S06P12A305-89 Test: 구독 관련 서비스 테스트 필드 추가 및 주석처리
    - Postman을 통해 기능 작동은 확인했지만, Test부분에서 오류 발생
    - 이후에 복합키 관련 기능들 구현 시 같은 문제가 발생한다면 문제 해결 후 같이 수정할 계획

--- backend/src/test/java/com/ssafy/bjbj/api/member/service/MemberServiceTest.java
+++ backend/src/test/java/com/ssafy/bjbj/api/member/service/MemberServiceTest.java
@@ -1,12 +1,17 @@
 package com.ssafy.bjbj.api.member.service;
 
 import com.ssafy.bjbj.api.member.dto.request.RequestMemberDto;
+import com.ssafy.bjbj.api.member.entity.Member;
+import com.ssafy.bjbj.api.member.entity.Subscribe;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -34,11 +39,52 @@ public class MemberServiceTest {
                         .password("test1234")
                         .name("홍길동")
                         .nickname("hong")
+                        .phoneNumber("010-1234-1234")
                         .build());
 
         // 회원 가입 후
         RequestMemberDto result2 = memberService.findMemberDtoByEmail(email1);
         assertThat(result2).isNotNull();
     }
-
+//
+//    @Test
+//    public void subscribeMemberTests() {
+//        //다른 회원 가입
+//        boolean saveMember1 = memberService.saveMember(RequestMemberDto.builder()
+//                .email("test@test.com")
+//                .password("test1234")
+//                .name("홍길동")
+//                .nickname("hong")
+//                .phoneNumber("010-1234-1234")
+//                .build());
+//        assertThat(saveMember1).isTrue();
+//
+//        Long fromMemberId = memberService.findMemberByEmail("test@test.com").getId();
+//
+//        boolean saveMember2 = memberService.saveMember(RequestMemberDto.builder()
+//                .email("test2@test.com")
+//                .password("test1234")
+//                .name("홍길동")
+//                .nickname("hongk")
+//                .phoneNumber("010-1234-1234")
+//                .build());
+//        assertThat(saveMember2).isTrue();
+//
+//        Long toMemberId = memberService.findMemberByEmail("test2@test.com").getId();
+//
+//        System.out.println("1");
+//        //구독 전
+//        boolean canSubscribe = memberService.subscribeMember(fromMemberId, toMemberId);
+//
+//        System.out.println("2");
+//        assertThat(canSubscribe).isTrue();
+//        //구독 후
+//        System.out.println("3");
+//
+//        boolean canSubscribe2 = memberService.subscribeMember(fromMemberId, toMemberId);
+//        assertThat(canSubscribe2).isFalse();
+//
+//
+//
+//    }
 }
