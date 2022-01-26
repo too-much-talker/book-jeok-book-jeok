db9b6b9ce6754a34b76f07f707d9d51f2774effe
김경석 <0xe82de@gmail.com>
2022-01-26 17:45:39 +0900

    #S06P12A305-31 Test: 경험치 조회 테스트 추가

--- backend/src/test/java/com/ssafy/bjbj/api/member/repository/MemberRepositoryTest.java
+++ backend/src/test/java/com/ssafy/bjbj/api/member/repository/MemberRepositoryTest.java
@@ -170,4 +170,31 @@ class MemberRepositoryTest {
         assertThat(point).isEqualTo(savedPoint);
     }
 
+    @DisplayName("ID로 Exp Select 테스트")
+    @Test
+    public void findExpById() {
+        // 회원가입
+        Integer exp = 1000;
+        Member member = Member.builder()
+                .email("bjbj@bjbj.com")
+                .password("test1234")
+                .name("홍길동")
+                .nickname("hong")
+                .phoneNumber("010-1234-5789")
+                .role(Role.MEMBER)
+                .exp(exp)
+                .point(100)
+                .build();
+
+        em.persist(member);
+        em.flush();
+        em.clear();
+
+        // exp select
+        Integer savedExp = memberRepository.findExpById(member.getId());
+
+        // 검증
+        assertThat(exp).isEqualTo(savedExp);
+    }
+
 }
