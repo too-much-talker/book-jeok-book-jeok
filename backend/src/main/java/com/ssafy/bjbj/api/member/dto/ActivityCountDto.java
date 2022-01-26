f1b3c138621e481fa74d40cb6923f0262b067a71
김경석 <0xe82de@gmail.com>
2022-01-26 17:43:08 +0900

    #S06P12A305-31 Feat: 활동량 카운트 Dto 필드 추가
    - 날짜, 카운트
    - Querydsl 생성자 추가

--- backend/src/main/java/com/ssafy/bjbj/api/member/dto/ActivityCountDto.java
+++ backend/src/main/java/com/ssafy/bjbj/api/member/dto/ActivityCountDto.java
@@ -1,8 +1,20 @@
 package com.ssafy.bjbj.api.member.dto;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.*;
 
 @ToString(of = {"date", "count"})
 @Getter
 public class ActivityCountDto {
+
+    private String date;
+
+    private int count;
+
+    @QueryProjection
+    public ActivityCountDto(String date, int count) {
+        this.date = date;
+        this.count = count;
+    }
+
 }
