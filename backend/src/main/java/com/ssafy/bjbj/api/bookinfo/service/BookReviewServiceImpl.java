9e84cf40bb24f38f20432941c35d3bf449e9f5ea
aletsire <aletsire@gmail.com>
2022-01-29 19:12:45 +0900

    #S06P12A305-99 Feat: 북리뷰 서비스 Impl 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/service/BookReviewServiceImpl.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/service/BookReviewServiceImpl.java
@@ -1,5 +1,13 @@
 package com.ssafy.bjbj.api.bookinfo.service;
 
+import com.ssafy.bjbj.api.bookinfo.dto.RequestBookReviewDto;
+import com.ssafy.bjbj.api.bookinfo.dto.ResponseBookReviewDto;
+import com.ssafy.bjbj.api.bookinfo.entity.BookInfo;
+import com.ssafy.bjbj.api.bookinfo.entity.BookReview;
+import com.ssafy.bjbj.api.bookinfo.repository.BookInfoRepository;
+import com.ssafy.bjbj.api.bookinfo.repository.BookReviewRepository;
+import com.ssafy.bjbj.api.member.entity.Member;
+import com.ssafy.bjbj.api.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,6 +19,51 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class BookReviewServiceImpl implements BookReviewService {
 
+    private final BookReviewRepository bookReviewRepository;
+
+    private final BookInfoRepository bookInfoRepository;
+
+    private final MemberRepository memberRepository;
+
+    @Transactional
+    @Override
+    public boolean registerReview(RequestBookReviewDto bookReviewDto) {
 
 
+        BookInfo bookInfo = bookInfoRepository.findBySeq(bookReviewDto.getBookInfoSeq());
+
+        Member member = memberRepository.findMemberBySeq(bookReviewDto.getMemberSeq());
+
+        ResponseBookReviewDto latestBookReviewDto = bookReviewRepository.findLatestBookReviewDtoByBookInfoAndMember(bookInfo.getSeq(), member.getSeq());
+
+        System.out.println("bookReviewRepository.findAll() = " + bookReviewRepository.findAll());
+        
+        if (latestBookReviewDto != null) {
+            if (!latestBookReviewDto.isDeleted()) {
+                BookReview bookReview = bookReviewRepository.findBySeq(latestBookReviewDto.getSeq());
+                bookReview.changeBookReviewDeleted(true);
+            }
+
+            bookReviewRepository.save(BookReview.builder()
+                    .bookInfo(bookInfo)
+                    .member(member)
+                    .starRating(bookReviewDto.getStarRating())
+                    .summary(bookReviewDto.getSummary())
+                    .isDeleted(false)
+                    .build());
+            System.out.println("bookReviewRepository = " + bookReviewRepository);
+
+            return false;
+        }
+
+        bookReviewRepository.save(BookReview.builder()
+                .bookInfo(bookInfo)
+                .member(member)
+                .starRating(bookReviewDto.getStarRating())
+                .summary(bookReviewDto.getSummary())
+                .isDeleted(false)
+                .build());
+
+        return true;
+    }
 }
