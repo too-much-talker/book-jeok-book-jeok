ca596855b0fcbd976ce66d7923de08e4b9d90c12
aletsire <aletsire@gmail.com>
2022-01-29 19:09:29 +0900

    #S06P12A305-99 Feat: 책정보 레포지토리 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookInfoRepository.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookInfoRepository.java
@@ -2,9 +2,12 @@ package com.ssafy.bjbj.api.bookinfo.repository;
 
 import com.ssafy.bjbj.api.bookinfo.entity.BookInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface BookInfoRepository extends JpaRepository<BookInfo, Long> {
 
-
+    @Query("SELECT m FROM BookInfo m WHERE m.seq = :seq")
+    BookInfo findBySeq(@Param("seq") Long seq);
 
 }
