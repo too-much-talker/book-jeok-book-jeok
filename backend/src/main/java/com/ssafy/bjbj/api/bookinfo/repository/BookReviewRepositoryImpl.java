3810d239bb14ec28d3b056631df564e319f8a9f1
aletsire <aletsire@gmail.com>
2022-01-29 18:39:56 +0900

    #S06P12A305-99 Create: 북리뷰 레포지토리Impl 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryImpl.java
@@ -0,0 +1,12 @@
+package com.ssafy.bjbj.api.bookinfo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.ssafy.bjbj.api.bookinfo.dto.QResponseBookReviewDto;
+import com.ssafy.bjbj.api.bookinfo.dto.ResponseBookReviewDto;
+
+import javax.persistence.EntityManager;
+import static com.ssafy.bjbj.api.bookinfo.entity.QBookReview.bookReview;
+
+public class BookReviewRepositoryImpl implements BookReviewRepositoryCustom {
+    
+}
