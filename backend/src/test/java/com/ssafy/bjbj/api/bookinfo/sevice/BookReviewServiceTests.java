2d992112dc1e8995aa9ebf0e15eb2e2fc1cd179c
aletsire <aletsire@gmail.com>
2022-01-29 19:15:06 +0900

    #S06P12A305-99 Test: 북리뷰 서비스 테스트 어노테이션 추가

--- backend/src/test/java/com/ssafy/bjbj/api/bookinfo/sevice/BookReviewServiceTests.java
+++ backend/src/test/java/com/ssafy/bjbj/api/bookinfo/sevice/BookReviewServiceTests.java
@@ -1,4 +1,10 @@
 package com.ssafy.bjbj.api.bookinfo.sevice;
 
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
 public class BookReviewServiceTests {
+
 }
