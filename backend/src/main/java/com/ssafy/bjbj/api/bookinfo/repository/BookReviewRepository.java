f77f9b2c8610beabc5af28ad5db4820dc784745a
aletsire <aletsire@gmail.com>
2022-01-29 19:09:52 +0900

    #S06P12A305-99 Feat: 북리뷰 레포지토리 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepository.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepository.java
@@ -1,10 +1,17 @@
 package com.ssafy.bjbj.api.bookinfo.repository;
 
+import com.ssafy.bjbj.api.bookinfo.entity.BookInfo;
 import com.ssafy.bjbj.api.bookinfo.entity.BookReview;
+import com.ssafy.bjbj.api.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface BookReviewRepository extends JpaRepository<BookReview, Long> {
+import java.util.List;
 
+public interface BookReviewRepository extends JpaRepository<BookReview, Long>, BookReviewRepositoryCustom {
 
+    @Query("SELECT m FROM BookReview m WHERE m.seq = :seq")
+    BookReview findBySeq(@Param("seq") Long seq);
 
 }
