7df037301054d4dd3b4dcef5204ef10ba227dc7d
aletsire <aletsire@gmail.com>
2022-01-29 12:08:59 +0900

    #S06P12A305-99 Create: 북리뷰 컨트롤러 생성

---
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/controller/BookReviewController.java
@@ -0,0 +1,16 @@
+package com.ssafy.bjbj.api.bookinfo.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/{bookinfo_seq}/reviews")
+@RestController
+public class BookReviewController {
+
+
+
+}
