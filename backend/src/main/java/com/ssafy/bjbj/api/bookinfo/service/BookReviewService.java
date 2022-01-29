f2f91ba355e56f9dbd021eed8c3c6408ee4d8860
aletsire <aletsire@gmail.com>
2022-01-29 19:12:23 +0900

    #S06P12A305-99 Feat: 북리뷰 서비스 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/service/BookReviewService.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/service/BookReviewService.java
@@ -1,5 +1,9 @@
 package com.ssafy.bjbj.api.bookinfo.service;
 
+import com.ssafy.bjbj.api.bookinfo.dto.RequestBookReviewDto;
+
 public interface BookReviewService {
+
+    boolean registerReview(RequestBookReviewDto requestBookReviewDto);
 
 }
