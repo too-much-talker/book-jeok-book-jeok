eba8d2676a97258208fe2cc1677ea94432a0b404
aletsire <aletsire@gmail.com>
2022-01-29 19:10:11 +0900

    #S06P12A305-99 Feat: 북리뷰 레포지토리 Custom 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryCustom.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryCustom.java
@@ -4,4 +4,5 @@ import com.ssafy.bjbj.api.bookinfo.dto.ResponseBookReviewDto;
 
 public interface BookReviewRepositoryCustom {
 
+    ResponseBookReviewDto findLatestBookReviewDtoByBookInfoAndMember(Long bookInfoSeq, Long memberSeq);
 }
