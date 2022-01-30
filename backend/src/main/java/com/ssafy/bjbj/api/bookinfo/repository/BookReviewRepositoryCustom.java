3264a2207d0e281dbf8c187b95e4abcd3b20f1a8
aletsire <aletsire@gmail.com>
2022-01-30 18:03:45 +0900

    #S06P12A305-108 Refactor: 북리뷰 레포지토리 Custom 필드 반환타입 수정

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryCustom.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryCustom.java
@@ -3,9 +3,11 @@ package com.ssafy.bjbj.api.bookinfo.repository;
 import com.ssafy.bjbj.api.bookinfo.dto.ResponseBookReviewDto;
 import com.ssafy.bjbj.api.bookinfo.entity.BookReview;
 
+import java.util.List;
+
 public interface BookReviewRepositoryCustom {
 
-    ResponseBookReviewDto findLatestBookReviewDtoByBookInfoAndMember(Long bookInfoSeq, Long memberSeq);
+    List<ResponseBookReviewDto> findAllBookReviewDtoByMemberSeq(Long memberSeq);
 
     BookReview findLatestBookReviewByBookInfoAndMember(Long bookInfoSeq, Long memberSeq);
 }
