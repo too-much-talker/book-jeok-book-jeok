8a3f86376e905ea142dbd95c57ee7276d57c907a
aletsire <aletsire@gmail.com>
2022-01-30 20:04:13 +0900

    #S06P12A305-48 Feat: BookReviewService 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/service/BookReviewService.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/service/BookReviewService.java
@@ -4,9 +4,13 @@ import com.ssafy.bjbj.api.bookinfo.dto.RequestBookReviewDto;
 import com.ssafy.bjbj.api.bookinfo.dto.ResponseBookReviewDto;
 import com.ssafy.bjbj.api.bookinfo.entity.BookReview;
 
+import java.util.List;
+
 public interface BookReviewService {
 
     BookReview findBookReviewByBookSeq(Long bookReviewSeq);
+
+    List<ResponseBookReviewDto> findAllBookReviewsByMemberSeq(Long memberSeq);
 
     ResponseBookReviewDto registerBookReview(RequestBookReviewDto requestBookReviewDto);
 
