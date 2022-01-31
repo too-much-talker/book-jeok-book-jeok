eb3742921ffd102ebf7cbf21df10ea6d22f7f7d9
김경석 <0xe82de@gmail.com>
2022-01-31 18:11:21 +0900

    #S06P12A305-38 Feat: 북로그 요청 DTO 필드 추가 및 빌더 생성자 추가

--- backend/src/main/java/com/ssafy/bjbj/api/booklog/dto/request/RequestBooklogDto.java
+++ backend/src/main/java/com/ssafy/bjbj/api/booklog/dto/request/RequestBooklogDto.java
@@ -1,11 +1,52 @@
 package com.ssafy.bjbj.api.booklog.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.validator.constraints.Range;
 
-@ToString(of = { "memberSeq", "booklogSeq", "title", "content", "starRating", "readDate", "isOpen"})
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+@ToString(of = { "memberSeq", "bookInfoSeq", "title", "content", "summary", "starRating", "readDate", "isOpen"})
 @NoArgsConstructor
 @Getter
 public class RequestBooklogDto {
+
+    @NotNull(message = "회원 식별키가 없습니다.")
+    private Long memberSeq;
+
+    @NotNull(message = "책 정보 식별키가 없습니다.")
+    private Long bookInfoSeq;
+
+    @NotBlank(message = "제목이 작성되지 않았습니다.")
+    private String title;
+
+    private String content;
+
+    private String summary;
+
+    @Range(min = 1, max = 5, message = "별점은 1, 2, 3, 4, 5점만 가능합니다.")
+    private Integer starRating;
+
+    @Pattern(regexp = "^(20[0-9][0-9]-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1]))?", message = "날짜 형식에 맞지 않습니다.")
+    private String readDate;
+
+    @NotNull(message = "공개여부가 설정되지 않았습니다.")
+    private Boolean isOpen;
+
+    @Builder
+    public RequestBooklogDto(Long memberSeq, Long bookInfoSeq, String title, String content, String summary, Integer starRating, String readDate, Boolean isOpen) {
+        this.memberSeq = memberSeq;
+        this.bookInfoSeq = bookInfoSeq;
+        this.title = title;
+        this.content = content;
+        this.summary = summary;
+        this.starRating = starRating;
+        this.readDate = readDate;
+        this.isOpen = isOpen;
+    }
+
 }
