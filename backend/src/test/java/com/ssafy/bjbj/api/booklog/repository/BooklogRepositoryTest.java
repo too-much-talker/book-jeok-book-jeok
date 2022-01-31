9c3f2f3edba6334b1991e4a1bc2e4736a0e0a214
김경석 <0xe82de@gmail.com>
2022-01-31 18:26:51 +0900

    #S06P12A305-38 Create: 북로그 레파지토리 테스트 클래스 생성

---
+++ backend/src/test/java/com/ssafy/bjbj/api/booklog/repository/BooklogRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.ssafy.bjbj.api.booklog.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@SpringBootTest
+class BooklogRepositoryTest {
+
+
+
+}
