7f7393b96e8993c86701bc4607536c9cef6193b9
aletsire <aletsire@gmail.com>
2022-02-01 15:40:33 +0900

    #S06P12A305-48 Refactor: BookReviewRepositoryCustom 필드 수정

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryCustom.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryCustom.java
@@ -1,13 +1,13 @@
 package com.ssafy.bjbj.api.bookinfo.repository;
 
-import com.ssafy.bjbj.api.bookinfo.dto.ResponseBookReviewDto;
+import com.ssafy.bjbj.api.bookinfo.dto.response.ResponseBookReviewByMemberDto;
 import com.ssafy.bjbj.api.bookinfo.entity.BookReview;
 
 import java.util.List;
 
 public interface BookReviewRepositoryCustom {
 
-    List<ResponseBookReviewDto> findAllBookReviewDtoByMemberSeq(Long memberSeq);
+    List<ResponseBookReviewByMemberDto> findAllBookReviewDtoByMemberSeq(Long memberSeq);
 
     BookReview findLatestBookReviewByBookInfoAndMember(Long bookInfoSeq, Long memberSeq);
 }
