8edac430e92f4f74913529e8935d162291361afd
aletsire <aletsire@gmail.com>
2022-02-02 22:34:52 +0900

    #S06P12A305-109 Test: BookReviewRepositoryTest 테스트 추가

--- backend/src/test/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryTests.java
+++ backend/src/test/java/com/ssafy/bjbj/api/bookinfo/repository/BookReviewRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.ssafy.bjbj.api.bookinfo.repository;
 
+import com.ssafy.bjbj.api.bookinfo.entity.BookInfo;
 import com.ssafy.bjbj.api.bookinfo.entity.BookReview;
 import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.api.member.entity.Role;
@@ -11,6 +12,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
+
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -51,6 +54,7 @@ public class BookReviewRepositoryTests {
         assertThat(bookReview.getSeq()).isEqualTo(savedBookReview.getSeq());
     }
 
+
     @Test
     public void findAllReviewBookDtoByMemberSeqTests() {
 
@@ -64,7 +68,6 @@ public class BookReviewRepositoryTests {
                 .exp(0)
                 .point(100)
                 .build();
-        
         em.persist(member);
         //북리뷰 작성 전
         assertThat(bookReviewRepository.findAllBookReviewDtoByMemberSeq(member.getSeq())).isEmpty();
@@ -90,4 +93,88 @@ public class BookReviewRepositoryTests {
         assertThat(bookReviewRepository.findAllBookReviewDtoByMemberSeq(member.getSeq()).stream().count()).isEqualTo(2);
 
     }
+
+    @Test
+    public void findAllReviewBookDtoByBookInfoSeqTests() {
+
+        String date = "2021-12-20T12:30:00";
+
+        String isbn = "isbn";
+        String title = "title";
+        String author = "author";
+        String description = "description";
+        Integer price = 100;
+        String smallImgUrl = "smallImgUrl";
+        String largeImgUrl = "largeImgUrl";
+        Integer categoryId = 101;
+        String categoryName = "categoryName";
+        String publisher = "publisher";
+        LocalDateTime publicationDate = LocalDateTime.parse(date);
+
+        BookInfo bookInfo = BookInfo.builder()
+                .isbn(isbn)
+                .title(title)
+                .author(author)
+                .description(description)
+                .price(price)
+                .smallImgUrl(smallImgUrl)
+                .largeImgUrl(largeImgUrl)
+                .categoryId(categoryId)
+                .categoryName(categoryName)
+                .publisher(publisher)
+                .publicationDate(publicationDate)
+                .build();
+
+        bookInfoRepository.save(bookInfo);
+
+        Member member1 = Member.builder()
+                .email("bjbj12@bjbj.com")
+                .password("test1234")
+                .name("홍길동")
+                .nickname("hongkar")
+                .phoneNumber("010-1234-3214")
+                .role(Role.MEMBER)
+                .exp(0)
+                .point(100)
+                .build();
+        Member member2 = Member.builder()
+                .email("bjbj13@bjbj.com")
+                .password("test1234")
+                .name("홍길동")
+                .nickname("hongkur")
+                .phoneNumber("010-1234-5152")
+                .role(Role.MEMBER)
+                .exp(0)
+                .point(100)
+                .build();
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //북리뷰 작성 전
+        Long bookInfoSeq = bookInfo.getSeq();
+        assertThat(bookReviewRepository.findAllBookReviewDtoByBookInfoSeq(bookInfoSeq)).isEmpty();
+
+        bookReviewRepository.save(BookReview.builder()
+                .bookInfo(bookInfoRepository.findBySeq(bookInfoSeq))
+                .member(member1)
+                .starRating(3)
+                .summary("테스트")
+                .isDeleted(false)
+                .build());
+
+        bookReviewRepository.save(BookReview.builder()
+                .bookInfo(bookInfoRepository.findBySeq(bookInfoSeq))
+                .member(member2)
+                .starRating(4)
+                .summary("테스트2")
+                .isDeleted(false)
+                .build());
+
+        em.flush();
+        em.clear();
+        //북리뷰 작성 후
+        assertThat(bookReviewRepository.findAllBookReviewDtoByBookInfoSeq(bookInfoSeq).stream().count()).isEqualTo(2);
+
+    }
+
 }
