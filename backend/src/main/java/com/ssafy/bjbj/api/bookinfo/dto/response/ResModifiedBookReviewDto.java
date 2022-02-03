1740d7c15529193e4287371df6b86bba001cb0b3
aletsire <aletsire@gmail.com>
2022-02-03 16:34:53 +0900

    #S06P12A305-52 Feat: ResModifiedBookReviewDto 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/api/bookinfo/dto/response/ResModifiedBookReviewDto.java
+++ backend/src/main/java/com/ssafy/bjbj/api/bookinfo/dto/response/ResModifiedBookReviewDto.java
@@ -13,4 +13,32 @@ import java.time.LocalDateTime;
 @Getter
 public class ResModifiedBookReviewDto {
 
+    private Long bookReviewSeq;
+
+    private Long bookInfoSeq;
+
+    private Long memberSeq;
+
+    private String memberNickname;
+
+    private Integer starRating;
+
+    private String summary;
+
+    private LocalDateTime createdDate;
+
+    private LocalDateTime modifiedDate;
+
+    @QueryProjection
+    @Builder
+    public ResModifiedBookReviewDto(Long bookReviewSeq,Long bookInfoSeq, Long memberSeq, String memberNickname, Integer starRating, String summary, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.bookReviewSeq = bookReviewSeq;
+        this.bookInfoSeq = bookInfoSeq;
+        this.memberSeq = memberSeq;
+        this.memberNickname = memberNickname;
+        this.starRating = starRating;
+        this.summary = summary;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
 }
