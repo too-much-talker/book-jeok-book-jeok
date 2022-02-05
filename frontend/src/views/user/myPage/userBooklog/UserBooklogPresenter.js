5561388af2e0bf98a53a9ac5ec7ef41c220b25c4
rohhy@naver.com <Srohhy@naver.com6>
2022-02-05 23:44:24 +0900

    0205

--- frontend/src/views/user/myPage/userBooklog/UserBooklogPresenter.js
+++ frontend/src/views/user/myPage/userBooklog/UserBooklogPresenter.js
@@ -4,18 +4,25 @@ import { useParams, Link } from "react-router-dom";
 
 function UserBooklogPresenter() {
   const { path } = useParams();
+
+  const Title = styled.div`
+    padding-left: 10px;
+  `;
+
   return (
     <div>
-      <div className="title">
+      <Title>
         <h2>나의 북로그</h2>
-      </div>
+      </Title>
       <div>
-        <Link to="/booklogdetail" state={{ logSeq: 1,
-        infoSeq:1 }}>
+        <Link to="/booklogdetail" state={{ logSeq: 1, infoSeq: 1 }}>
           <div>북로그1</div>
         </Link>
         <Link to="/booklogregister">
-          <button style={{ position: "absolute", right: 0, marginRight: "150px" }}>작성</button>
+          <button
+            style={{ position: "absolute", right: 0, marginRight: "20px" }}>
+            작성
+          </button>
         </Link>
       </div>
       <div>{/* <BoardList /> */}</div>
