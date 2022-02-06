da6dd9b6b7bf9dc5f4c66938971f45528b2682b5
unain-dev <daeun84366@gmail.com>
2022-02-06 22:38:51 +0900

    #fe/feature/booklog/#S06P12A305-121 Feat: 공개 북로그 검색 기능

--- frontend/src/views/booklogs/booklogList/BooklogListContainer.js
+++ frontend/src/views/booklogs/booklogList/BooklogListContainer.js
@@ -1,12 +1,16 @@
 import BooklogListPresenter from "./BooklogListPresenter";
 import { useEffect, useState } from "react";
-import { booklogList } from "../../../common/api/booklog";
+import { booklogList, findBooklog } from "../../../common/api/booklog";
 import "./Paging.css";
 import Pagination from "react-js-pagination";
 import { useParams } from "react-router-dom";
 import SearchbarContainer from "./SearchbarContainer";
+import QueryString from "qs";
+import { useLocation } from "react-router";
 
 function BooklogListContainer() {
+  const location = useLocation(); //바뀐 부분
+
   const [order, setOrder] = useState("");
   const params = useParams().order;
   if (params !== order) {
@@ -21,24 +25,48 @@ function BooklogListContainer() {
 
   const [totalPage, setTotalPage] = useState(1);
   const size = 15;
+  const [totalCnt, setTotalCnt] = useState(0);
 
   useEffect(() => {
-    booklogList(
-      {
-        page: page,
-        size: size,
-        sort: order,
-      },
-      (response) => {
-        console.log(response);
-        setTotalPage(response.data.data.totalPage);
-        setData(response.data.data.booklogs);
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }, [page, order, totalPage]);
+    if (order !== "search") {
+      booklogList(
+        {
+          page: page,
+          size: size,
+          sort: order,
+        },
+        (response) => {
+          console.log(response);
+          setTotalPage(response.data.data.totalPage);
+          setData(response.data.data.booklogs);
+          setTotalCnt(response.data.data.totalCnt);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    } else if (order === "search") {
+      const queryData = QueryString.parse(location.search, {
+        ignoreQueryPrefix: true,
+      });
+      findBooklog(
+        {
+          page: page,
+          size: size,
+          ...queryData,
+        },
+        (response) => {
+          console.log(response);
+          setTotalPage(response.data.data.totalPage);
+          setData(response.data.data.booklogs);
+          setTotalCnt(response.data.data.totalCnt);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+  }, [page, order, totalPage, location]);
 
   useEffect(() => {
     setPage(1);
@@ -48,11 +76,16 @@ function BooklogListContainer() {
     <>
       <h1>북로그 트랜드</h1>
       <SearchbarContainer />
-      <BooklogListPresenter data={data} isPopular={order === "like"} />
+      <BooklogListPresenter
+        order={order}
+        data={data}
+        isPopular={order === "like"}
+        totalCnt={totalCnt}
+      />
       <Pagination
         activePage={page}
         itemsCountPerPage={size}
-        totalItemsCount={totalPage * size}
+        totalItemsCount={totalCnt}
         pageRangeDisplayed={5}
         prevPageText={"‹"}
         nextPageText={"›"}
