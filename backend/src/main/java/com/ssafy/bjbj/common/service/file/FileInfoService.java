4014a2ff96f26f2ffe0f5118f316c0ad47d033c6
aletsire <aletsire@gmail.com>
2022-02-08 22:47:13 +0900

    #S06P12A305-59 Refactor: FileInfoService 필드 추가

--- backend/src/main/java/com/ssafy/bjbj/common/service/file/FileInfoService.java
+++ backend/src/main/java/com/ssafy/bjbj/common/service/file/FileInfoService.java
@@ -1,10 +1,17 @@
 package com.ssafy.bjbj.common.service.file;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import java.util.List;
 
 public interface FileInfoService {
 
+    void registerFileInfo(Long rootSeq,List<MultipartFile> files) throws Exception;
+
     List<String> findAllFileInfoByReadingGroupBoardSeq(Long readingGroupBoardSeq);
 
+    void updateFileInfo(Long rootSeq, List<MultipartFile> files) throws Exception;
+
     void deleteFileInfo(Long rootSeq, Long memberSeq);
+
 }
