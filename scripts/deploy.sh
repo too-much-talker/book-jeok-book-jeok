6c29f79af0503e29eb1a0547bddd1eec36beac92
김경석 <0xe82de@gmail.com>
2022-02-09 17:48:16 +0900

    #S06P12A305-173 Refactor: 에러 출력 리디렉션 추가

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,8 +7,8 @@ echo "##### 배포 시작 #####"
 echo "#####################"
 echo ""
 
-sh $BUILD_SCRIPT_PATH/frontend-deploy.sh
-sh $BUILD_SCRIPT_PATH/backend-deploy.sh
+sh $BUILD_SCRIPT_PATH/frontend-deploy.sh 2>&1
+sh $BUILD_SCRIPT_PATH/backend-deploy.sh 2>&1
 
 echo "#####################"
 echo "##### 배포 종료 #####"
