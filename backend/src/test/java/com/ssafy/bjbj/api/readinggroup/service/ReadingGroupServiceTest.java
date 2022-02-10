0bdee69bafd3f4f5f6a735d5c37d1ba823385044
김경석 <0xe82de@gmail.com>
2022-02-10 16:42:52 +0000

    [BE: D22] #S06P12A305-152 독서 모임 포스팅 상세페이지 조회

--- backend/src/test/java/com/ssafy/bjbj/api/readinggroup/service/ReadingGroupServiceTest.java
+++ backend/src/test/java/com/ssafy/bjbj/api/readinggroup/service/ReadingGroupServiceTest.java
@@ -5,10 +5,12 @@ import com.ssafy.bjbj.api.member.entity.Member;
 import com.ssafy.bjbj.api.member.entity.Role;
 import com.ssafy.bjbj.api.member.repository.MemberRepository;
 import com.ssafy.bjbj.api.readinggroup.dto.request.ReqReadingGroupDto;
+import com.ssafy.bjbj.api.readinggroup.dto.response.ResReadingGroupDetailDto;
 import com.ssafy.bjbj.api.readinggroup.entity.ReadingGroup;
 import com.ssafy.bjbj.api.readinggroup.entity.ReadingGroupDate;
 import com.ssafy.bjbj.api.readinggroup.entity.ReadingGroupMember;
 import com.ssafy.bjbj.api.readinggroup.repository.ReadingGroupRepository;
+import com.ssafy.bjbj.common.enums.Day;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,6 +25,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -136,6 +139,42 @@ class ReadingGroupServiceTest {
         List<ReadingGroupMember> readingGroupMembers = findReadingGroup.getReadingGroupMembers();
         assertThat(readingGroupMembers.size()).isEqualTo(1);
         assertThat(readingGroupMembers.get(0).getMember().getSeq()).isEqualTo(member1.getSeq());
+    }
+
+    @DisplayName("독서 모임 모집 포스팅 상세페이지 Dto get 서비스 메서드 테스트")
+    @Test
+    public void readingGroupDetailDtoGetTest() {
+        memberRepository.save(member1);
+
+        ReadingGroup savedReadingGroup = readingGroupService.register(reqReadingGroupDto1, member1.getSeq());
+        em.flush();
+        em.clear();
+
+        ResReadingGroupDetailDto resDto1 = readingGroupService.getResDto(savedReadingGroup.getSeq());
+
+        assertThat(resDto1.getReadingGroupSeq()).isEqualTo(savedReadingGroup.getSeq());
+        assertThat(resDto1.getWriter()).isEqualTo(savedReadingGroup.getMember().getNickname());
+        assertThat(resDto1.getTitle()).isEqualTo(reqReadingGroupDto1.getTitle());
+        assertThat(resDto1.getContent()).isEqualTo(reqReadingGroupDto1.getContent());
+        assertThat(resDto1.getMinLevel()).isEqualTo(reqReadingGroupDto1.getLimitLevel());
+        assertThat(resDto1.getMaxNumOfMember()).isEqualTo(reqReadingGroupDto1.getMaxMember());
+        assertThat(resDto1.getViews()).isEqualTo(savedReadingGroup.getViews());
+        assertThat(resDto1.getStatus()).isEqualTo(savedReadingGroup.getStatus());
+        assertThat(resDto1.getDeadline()).isEqualTo(reqReadingGroupDto1.getDeadline());
+        assertThat(resDto1.getStartDate()).isEqualTo(reqReadingGroupDto1.getStartDate());
+        assertThat(resDto1.getEndDate()).isEqualTo(reqReadingGroupDto1.getEndDate());
+        assertThat(resDto1.getReadingGroupType().toString()).isEqualToIgnoringCase(reqReadingGroupDto1.getReadingGroupType());
+        assertThat(resDto1.getCreatedDate()).isEqualTo(savedReadingGroup.getCreatedDate().toLocalDate());
+        assertThat(resDto1.getParticipants().size()).isEqualTo(1);
+        assertThat(resDto1.getParticipants().get(0)).isEqualTo(savedReadingGroup.getMember().getNickname());
+        assertThat(resDto1.getDays().size()).isEqualTo(reqReadingGroupDto1.getDays().size());
+
+        List<String> days = reqReadingGroupDto1.getDays();
+        days = days.stream().map(String::toUpperCase)
+                .collect(Collectors.toList());
+        for (Day day : resDto1.getDays()) {
+            assertThat(days).contains(day.toString());
+        }
     }
 
 }
