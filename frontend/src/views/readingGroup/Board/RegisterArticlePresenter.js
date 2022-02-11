e30b69b6a768a5f419f060eb729c59e50d4e20e5
김수민 <jamie1541@naver.com>
2022-02-11 12:57:39 +0900

    #S06P12A305-135 Refactor: 사진 여러개 업로드 가능

--- frontend/src/views/readingGroup/Board/RegisterArticlePresenter.js
+++ frontend/src/views/readingGroup/Board/RegisterArticlePresenter.js
@@ -5,23 +5,56 @@ import styled from "styled-components";
 //   method:"post",
 //    enctype:"multipart/form-data"
 // }))``;
-const Title= styled.input``;
+const Title = styled.input``;
 const Content = styled.input``;
-const Image= styled.input.attrs(props => ({
-    type: "file",
-    id:"file-id"
-  }))``;
-const SubmitBtn = styled.button``;
-function RegisterArticlePresenter({onCreate,handleTitle,handleContent,handleImage}){
- 
-    return(
+// const Image = styled.input.attrs((props) => ({
+//   type: "file",
+//   id: "file-id",
+//   accept: "image/*",
+// }))``;
+const Image = styled.div``;
+const SubmitBtn = styled.button`
+disble
+`;
+const Preview = styled.div``;
+const Message = styled.div``;
+function RegisterArticlePresenter({
+  button,
+  postfiles,
+  onCreate,
+  handleTitle,
+  handleContent,
+  uploadFile,
+}) {
+  return (
     // <Form>
-      <>
-    <Title onChange={handleTitle} placeholder="제목을 입력해주세요"></Title>
-    <Content onChange={handleContent} placeholder="내용을 입력해주세요"></Content>
-    <Image onChange={handleImage}></Image>
-    <SubmitBtn onClick={onCreate}>작성 완료</SubmitBtn>
+    <>
+      <Title onChange={handleTitle} placeholder="제목을 입력해주세요"></Title>
+      <Content
+        onChange={handleContent}
+        placeholder="내용을 입력해주세요"
+      ></Content>
+      <Image>
+        <input
+          id="upload-file"
+          type="file"
+          accept="image/*, video/*"
+          multiple
+          onChange={uploadFile}
+        ></input>
+      </Image>
+      <Message>
+        사진을 여러개 선택하고 싶을 경우 한번에 선택해주세요.(최대 5장))
+      </Message>
+      {/* <Image onChange={uploadFile} multiple></Image> */}
+      <Preview>
+        {postfiles.file !== null ? <img src={postfiles.previewURL} /> : null}
+      </Preview>
+      <SubmitBtn onClick={onCreate} disabled={button}>
+        작성 완료
+      </SubmitBtn>
     </>
     // </Form>);
-    )}
+  );
+}
 export default RegisterArticlePresenter;
