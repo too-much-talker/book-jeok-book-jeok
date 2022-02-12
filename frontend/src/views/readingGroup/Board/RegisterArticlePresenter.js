70e66e824fda3f38e3df6152f8d4693f1c81ab8e
김수민 <jamie1541@naver.com>
2022-02-12 15:27:28 +0900

    #S06P12A305-182 Style: 게시판 게시글 작성 디자인 수정

--- frontend/src/views/readingGroup/Board/RegisterArticlePresenter.js
+++ frontend/src/views/readingGroup/Board/RegisterArticlePresenter.js
@@ -6,19 +6,74 @@ import Slider from "react-slick";
 //   method:"post",
 //    enctype:"multipart/form-data"
 // }))``;
-const Title = styled.input``;
-const Content = styled.input``;
-// const Image = styled.input.attrs((props) => ({
-//   type: "file",
-//   id: "file-id",
-//   accept: "image/*",
-// }))``;
-const Image = styled.div``;
-const SubmitBtn = styled.button`
-disble
+const Block = styled.div`
+  height: 100vh;
 `;
-const Preview = styled.div``;
-const Message = styled.div``;
+const Article = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100%;
+  border: 1px solid black;
+  border-radius: 20px;
+`;
+const TitleBlock = styled.div`
+  margin-top: 5%;
+  position: relative;
+  height: 5%;
+  width: 80%;
+  margin-left: 10%;
+  text-align: left;
+`;
+const Title = styled.input`
+  position: relative;
+  margin-left: 3%;
+  width: 90%;
+  height: 100%;
+`;
+const ContentBlock = styled.div`
+  margin-top: 3%;
+  position: relative;
+  height: 30%;
+  width: 80%;
+  margin-left: 10%;
+  text-align: left;
+`;
+const Content = styled.input`
+  position: relative;
+  margin-left: 3%;
+  width: 90%;
+  height: 100%;
+`;
+const Image = styled.div`
+  position: relative;
+  text-align: left;
+  margin-left: 15%;
+  margin-top: 3%;
+`;
+const SubmitBtn = styled.button`
+  position: relative;
+  height: 5%;
+  width: 10%;
+  border: 1px;
+  background: black;
+  color: white;
+  border-radius: 20px;
+  margin-top: 1%;
+`;
+const Preview = styled.div`
+  position: relative;
+  text-align: left;
+  margin-left: 15%;
+  margin-top: 1%;
+`;
+
+const Message = styled.div`
+  position: relative;
+  text-align: left;
+  margin-left: 15%;
+  margin-top: 1%;
+`;
+
 function RegisterArticlePresenter({
   button,
   postfiles,
@@ -28,34 +83,44 @@ function RegisterArticlePresenter({
   uploadFile,
 }) {
   return (
-    // <Form>
-    <>
-      <Title onChange={handleTitle} placeholder="제목을 입력해주세요"></Title>
-      <Content
-        onChange={handleContent}
-        placeholder="내용을 입력해주세요"
-      ></Content>
-      <Image>
-        <input
-          id="upload-file"
-          type="file"
-          accept="image/*, video/*"
-          multiple
-          onChange={uploadFile}
-        ></input>
-      </Image>
-      <Message>
-        사진을 여러개 선택하고 싶을 경우 한번에 선택해주세요.(최대 5장))
-      </Message>
-      {/* <Image onChange={uploadFile} multiple></Image> */}
-      <Preview>
-        {postfiles.file !== null ? <img src={postfiles.previewURL} /> : null}
-      </Preview>
-      <SubmitBtn onClick={onCreate} disabled={button}>
-        작성 완료
-      </SubmitBtn>
-    </>
-    // </Form>);
+    <Block>
+      <Article>
+        <TitleBlock>
+          <label>제목</label>
+          <Title
+            onChange={handleTitle}
+            placeholder="제목을 입력해주세요"
+          ></Title>
+        </TitleBlock>
+        <ContentBlock>
+          <label>내용</label>
+          <Content
+            onChange={handleContent}
+            placeholder="내용을 입력해주세요"
+          ></Content>
+        </ContentBlock>
+        <Image>
+          <input
+            id="upload-file"
+            type="file"
+            accept="image/*, video/*"
+            multiple
+            onChange={uploadFile}
+          ></input>
+        </Image>
+        <Message>
+          사진을 여러개 선택하고 싶을 경우 한번에 선택해주세요.(최대 5장)
+        </Message>
+        <Preview>
+          {postfiles.file !== null ? (
+            <img src={postfiles.previewURL} width="300px" height="200px" />
+          ) : null}
+        </Preview>
+        <SubmitBtn onClick={onCreate} disabled={button}>
+          작성 완료
+        </SubmitBtn>
+      </Article>
+    </Block>
   );
 }
 export default RegisterArticlePresenter;
