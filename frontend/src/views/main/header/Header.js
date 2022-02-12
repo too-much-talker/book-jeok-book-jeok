dfc8de1e7523cd01190c518e54572ef6f41ce057
unain-dev <daeun84366@gmail.com>
2022-02-13 00:55:35 +0900

    #fe/refactor/member/#S06P12A305-114 Feat: jwtToken 없을 때 store 초기화

--- frontend/src/views/main/header/Header.js
+++ frontend/src/views/main/header/Header.js
@@ -3,7 +3,9 @@ import styled from "styled-components";
 import logo from "../../../res/img/logo.png";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setUserInfo } from "../../../common/reducers/modules/auth";
 
 const HeaderWrapper = styled.div`
   width: 1170px;
@@ -31,6 +33,29 @@ function Header() {
   // const { jwtToken, memberInfo } = useSelector((state) => state.authReducer);
   // console.log(jwtToken, memberInfo);
   const jwtToken = JSON.parse(sessionStorage.getItem("jwtToken"));
+  const dispatch = useDispatch();
+
+  const memberInfo = useSelector((state) => state.authReducer.memberInfo);
+
+  useEffect(() => {
+    if (
+      (jwtToken === null || jwtToken === undefined || jwtToken === "") &&
+      (memberInfo !== null || memberInfo !== undefined || memberInfo !== "")
+    ) {
+      dispatch(
+        setUserInfo({
+          memberInfo: {
+            seq: "",
+            email: "",
+            password: "",
+            name: "",
+            nickname: "",
+          },
+          jwtToken: "",
+        })
+      );
+    }
+  }, []);
 
   return (
     <HeaderRoot>
