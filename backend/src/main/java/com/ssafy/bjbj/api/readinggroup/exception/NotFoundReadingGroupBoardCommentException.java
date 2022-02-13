1e5bbbb5971fcea7ad47e6a243bd74eb5213de3e
aletsire <aletsire@gmail.com>
2022-02-13 20:16:51 +0900

    Revert "Revert "#S06P12A305-129 Feat: 독서 모임별 게시판 게시글 댓글 삭제""
    This reverts commit e985030531a06b094db4eacbd698a2f7d8189165.

---
+++ backend/src/main/java/com/ssafy/bjbj/api/readinggroup/exception/NotFoundReadingGroupBoardCommentException.java
@@ -0,0 +1,8 @@
+package com.ssafy.bjbj.api.readinggroup.exception;
+
+public class NotFoundReadingGroupBoardCommentException extends RuntimeException{
+
+    public NotFoundReadingGroupBoardCommentException(String message) {
+        super(message);
+    }
+}
