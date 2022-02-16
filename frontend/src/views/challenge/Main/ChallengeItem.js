8403d51d8c42ed979114f2b1a48d422f9de3cd2a
김수민 <jamie1541@naver.com>
2022-02-16 14:12:32 +0900

    temp

--- frontend/src/views/challenge/Main/ChallengeItem.js
+++ frontend/src/views/challenge/Main/ChallengeItem.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const Block = styled.div`
   position: relative;
   width: 100%;
-  height: 100%;
+  marginl
   margin-bottom: 50px;
   margin: 15px;
   &:hover {
