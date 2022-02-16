1ff1824bfb44300f3fc7668b87f45ca4e4319ee7
김경석 <0xe82de@gmail.com>
2022-02-16 21:46:07 +0900

    #S06P12A305-212 Refactor: 나의 챌린지 목록 조회 API 리팩토링
    API를 수정하였습니다.
    - 나의 챌린지 목록 조회 시에 챌린지 별 참여인원 수, 리워드, 달성률, 진행 상태를 추가 반환합니다.

--- backend/src/main/java/com/ssafy/bjbj/api/challenge/dto/response/MyChallengeDto.java
+++ backend/src/main/java/com/ssafy/bjbj/api/challenge/dto/response/MyChallengeDto.java
@@ -1,6 +1,7 @@
 package com.ssafy.bjbj.api.challenge.dto.response;
 
 import com.querydsl.core.annotations.QueryProjection;
+import com.ssafy.bjbj.common.enums.Status;
 import lombok.Getter;
 import lombok.ToString;
 
@@ -19,12 +20,27 @@ public class MyChallengeDto {
 
     private LocalDate endDate;
 
+    private int numOfParticipants;
+
+    private int maxMember;
+
+    private int reward;
+
+    private Status status;
+
+    private double authRate;
+
     @QueryProjection
-    public MyChallengeDto(Long challengeSeq, String title, LocalDateTime startDate, LocalDateTime endDate) {
+    public MyChallengeDto(Long challengeSeq, String title, LocalDateTime startDate, LocalDateTime endDate, int numOfParticipants, int maxMember, int reward, Status status, double authRate) {
         this.challengeSeq = challengeSeq;
         this.title = title;
         this.startDate = startDate.toLocalDate();
         this.endDate = endDate.toLocalDate();
+        this.numOfParticipants = numOfParticipants;
+        this.maxMember = maxMember;
+        this.reward = reward;
+        this.status = status;
+        this.authRate = authRate;
     }
 
 }
--- backend/src/main/java/com/ssafy/bjbj/api/challenge/service/ChallengeServiceImpl.java
+++ backend/src/main/java/com/ssafy/bjbj/api/challenge/service/ChallengeServiceImpl.java
@@ -23,7 +23,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
